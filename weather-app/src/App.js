4fc86b9e6acc09bff01d930f716fc847830a7ab6
Masha <4uikina@gmail.com>
2020-02-05 13:14:39 +0100

    till 1h ERR_NAME_NOT_RESOLVED error

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,6 +6,14 @@ import Weather from './components/weather'
 const API_KEY = '912f70c673957ab4aff19036d729afc0';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
 
   getWeather = async (e) => {
     e.preventDefault();
@@ -13,7 +21,26 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    if (city && country) {
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
+  } else {
+    this.setState({
+      temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: 'Please enter the values'
+  })
+}
   }
 
 
@@ -22,7 +49,13 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error} />
         
 
         </div>

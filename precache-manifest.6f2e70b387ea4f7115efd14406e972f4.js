4d8735860b96c29dc93e25881ce85ae54c00a865
Masha <4uikina@gmail.com>
2020-02-07 21:09:57 +0100

    Updates

---
+++ precache-manifest.6f2e70b387ea4f7115efd14406e972f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2429eeb3fd239b8cccb34bda7642ea88",
+    "url": "/react-week-1-project/index.html"
+  },
+  {
+    "revision": "95df8c1e2b8f76673f99",
+    "url": "/react-week-1-project/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "95df8c1e2b8f76673f99",
+    "url": "/react-week-1-project/static/js/2.a6a2ffc4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-week-1-project/static/js/2.a6a2ffc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba4fb22e6d7b627764a",
+    "url": "/react-week-1-project/static/js/main.df96370d.chunk.js"
+  },
+  {
+    "revision": "63a3cdd9746ca4978196",
+    "url": "/react-week-1-project/static/js/runtime-main.9dadbc24.js"
+  }
+]);
